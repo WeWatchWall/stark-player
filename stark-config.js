41e79860a20168d75811070ccb99fb48ca2157ea
Adrian Burlacu <adrian.burlacu@live.com>
2021-09-07 18:27:40 +0300

    Stark-config and sequencer version.

---
+++ stark-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "availability": 2,
+  "security": 0,
+  "tags": [],
+  "maxPods": 0,
+  "numPods": 0,
+  "status": 0,
+  "isService": false,
+  "services": [],
+  "nodePods": {},
+  "arg": {},
+  "sandbox": 2,
+  "runtime": 0,
+  "mode": 2
+};
