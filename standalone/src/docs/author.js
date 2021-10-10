a1adce3f03ae7e58ef4935384761e166b70ee6b7
Adrian Burlacu <adrian.burlacu@live.com>
2021-10-10 09:33:06 +0300

    Playlist link.

--- standalone/src/docs/author.js
+++ standalone/src/docs/author.js
@@ -1,7 +1,7 @@
 export default {
   part1: `
     <ol>
-    <li>This section describes the creation process for WWWall-compatible playlists. You can <a href="https://github.com/WeWatchWall/stark-player/blob/main/standalone/example_playlists/playlist.zip" target="_blank">download the example playlist here</a>.</li>
+    <li>This section describes the creation process for WWWall-compatible playlists. You can <a href="https://github.com/WeWatchWall/stark-player/blob/main/standalone/example_playlists/playlist.zip?raw=true" target="_blank">download the example playlist here</a>.</li>
     <li>The playlist file is a .zip file with <strong>.mp3</strong> music files and a <strong>.pls</strong> file that describes their order and holds other metadata. <a href="https://www.thewindowsclub.com/what-is-a-pls-file" target="_blank">You can use this guide to get started on creating a .pls file</a>.</li>
     <li>Ensure that each item in the playlist has the following properties: FileN, TitleN, LengthN (in seconds) where N is the order index of the item.</li>
     <li>As is the case with the example playlist, manual synchronization can be specified by adding this line at the end of the playlist file:</li>
