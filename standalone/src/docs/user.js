acfde13bf8aa6c29e2bb4df35893f8d5d9ed97a6
Adrian Burlacu <adrian.burlacu@live.com>
2021-10-10 09:30:48 +0300

    Example playlist link.

--- standalone/src/docs/user.js
+++ standalone/src/docs/user.js
@@ -5,7 +5,7 @@ export default `
   <li>The best browser to use is the <a href="https://brave.com/" target="_blank">Brave Browser</a>. If on mobile, enable the "Background video playback" option. Chrome is also a good alternative.</li>
   <li>First, make sure that your device clock is synchronized to the internet for accuracy.</li>
   <li>Usually, your event organizer will send you a link with a file or a website with a playlist .zip file. <strong>For your security, only download files from trusted websites and email senders.</strong></li>
-  <li>If you need to create a playlist, you can check the "Author Info" section of this app, or <a href="https://github.com/WeWatchWall/stark-player/blob/main/example_playlists/playlist_medium.zip" target="_blank">download the example playlist</a>.</li>
+  <li>If you need to create a playlist, you can check the "Author Info" section of this app, or <a href="https://github.com/WeWatchWall/stark-player/blob/main/standalone/example_playlists/playlist.zip" target="_blank">download the example playlist</a>.</li>
   <li>Unlike other playlists, every device needs to load the example playlist within the same minute.</li>
   <li>When you get to the event, select the file on the "Play" page of the app.</li>
   <li>Then, follow the on-screen instructions.</li>
